21e02fae253a12786a3db5bf1fcacf640a04dbd0
Lucas Rosenzweig <38187137+Lucas-Rosenzweig@users.noreply.github.com>
2025-10-06 13:53:11 +0200

    Add configuration file for Discord bot settings

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    app: {
+        token: "DISCORD-TOKEN-HERE",
+        clientId: "DISCORD-CLIENT-ID",
+        guild: "DISCORD_GUILD-ID",
+        global: true,
+        color: "#af33c1",
+        youtube_token: "GOOGLE-ACCES-TOKEN"
+    },
+    messages: {
+        userNotInVoiceChannel: "Vous devez   tre dans un salon vocal pour utiliser cette commande !",
+        voiceChannelNotJoinable: "Je ne peux pas rejoindre votre salon vocal !",
+        botCannotTalk: "Je n'ai pas la permission de parler dans votre salon vocal !",
+        nothingToPlayNext: "Je n'ai plus rien    jouer !",
+        leavingChannel: "Je m'en vais !",
+        queueShuffle: "La file d'attente a été   mélangée !",
+        noMusicPlaying: "Il n'y a pas de musique en cours de lecture !",
+        error: "Une erreur est survenue lors de l'ex  cution de la commande.",
+    }
+}
